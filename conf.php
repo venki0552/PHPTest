19f43425b2c0027a1d9ebcc5e1e4b2d149503af1
venki0552 <venkionfire@gmail.com>
2014-04-06 22:04:40 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,13 +1,24 @@
 <?php
-        define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-        define('DB_USER','adminWIugTmz');
-        define('DB_PASS','MXCtu7sHtApv');
-        define('DB_BASE','phptest');
-        define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-function mysqlConnector(){
-    $dsn = 'mysql:dbname='.DB_BASE.';host='.DB_HOST.';port='.DB_PORT;
-    $dbh = new PDO($dsn, DB_USER, DB_PASS);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbh;
-}
-         ?>
+ define( "DB_SERVER",    getenv('OPENSHIFT_MYSQL_DB_HOST') );
+ 
+ define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
+ 
+ define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
+ 
+ define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
+ 
+ 
+ mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
+mysql_select_db(DB_DATABASE) or die(mysql_error());
+ 
+ $q = "select * from salary";
+ $result = mysql_query($q) or die(mysql_error());
+ 
+ while ($row = mysql_fetch_array($result))
+         {
+                 echo $row['eno'];
+                 echo $row['fname'];
+                 echo $row['salary'];
+         }
+mysql_close();
+?>

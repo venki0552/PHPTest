c7192d26ae86b1b66e55d958feca19c633e2be2f
venki0552 <venkionfire@gmail.com>
2014-04-10 13:29:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
  try {
+    $id = $_POST['id'];
+  
                 require_once 'conf.php';
                 $conn = mysqlConnector();
-                $stmt = $conn->prepare('SELECT * FROM testing');
+                $stmt = $conn->prepare('SELECT * FROM testing where id'+$id);
                 $stmt->execute();
 
                 while($row = $stmt->fetch()) {

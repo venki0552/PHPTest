29ff30db565e92147f6424cfb62f79c7ea4bab18
venki0552 <venkionfire@gmail.com>
2014-04-10 13:27:12 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -5,7 +5,7 @@
  
  define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
  
- define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
+ define( "DB_DATABASE", 'venkat' );
  
  
  mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());

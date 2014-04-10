a4b09013d8221922f5181a08f0d5f7ab7e402c7f
venki0552 <venkionfire@gmail.com>
2014-04-10 13:26:31 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -11,14 +11,5 @@
  mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
 mysql_select_db(DB_DATABASE) or die(mysql_error());
  
- $q = "select * from salary";
- $result = mysql_query($q) or die(mysql_error());
- 
- while ($row = mysql_fetch_array($result))
-         {
-                 echo $row['eno'];
-                 echo $row['fname'];
-                 echo $row['salary'];
-         }
-mysql_close();
+
 ?>
